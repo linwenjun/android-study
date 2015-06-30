b5da72f15ed3399e689aba4ae649c39a0d5bdd89
Wenjun LIN <wenjun.lin@rea-group.com>
2015-06-30 21:09:03 +0800

    add call back for new components

--- app/src/main/java/thoughtworks/academy/myandroiddemo/NumberAdderLayout.java
+++ app/src/main/java/thoughtworks/academy/myandroiddemo/NumberAdderLayout.java
@@ -1,6 +1,8 @@
 package thoughtworks.academy.myandroiddemo;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,32 +17,55 @@ public class NumberAdderLayout extends LinearLayout{
     }
 
     Button addButton = null;
-    TextView number = null;
-    ICallback iCallback = null;
+    TextView numberText = null;
+    ICallback callback = null;
+    int number;
+
+    Handler handler = null;
 
     public NumberAdderLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.number_adder, this);
 
         addButton = (Button) findViewById(R.id.add_button);
-        number = (TextView) findViewById(R.id.number);
+        numberText = (TextView) findViewById(R.id.number);
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message message) {
+                callback.onNumberChange(message.what);
+                return false;
+            }
+        });
 
         addButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                int currentValue = getNumber();
-                number.setText((++currentValue) + "");
+                number = getNumber();
+                number++;
+                numberText.setText(number + "");
 
-                iCallback.onNumberChange(currentValue);
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        handler.sendEmptyMessage(number);
+                    }
+                }.start();
+
             }
         });
     }
 
     public void setCallBack(ICallback callBack) {
-        iCallback = callBack;
+        callback = callBack;
     }
 
     private int getNumber() {
-        return Integer.parseInt(number.getText().toString());
+        return Integer.parseInt(numberText.getText().toString());
     }
 }
